18f50e488f6a2a65de68d808b583b7a3b7fb49d3
makebanana <530547479@qq.com>
2018-02-27 00:37:26 +0800

    [coding] remove comment

--- src/Object/deepClone.js
+++ src/Object/deepClone.js
@@ -5,9 +5,9 @@ var deepClone = function fnDeepClone(obj){
     result = typeof obj.splice === 'function' ? [] : {};
     for (key in obj ){
       if (obj[key] && typeof obj[key] === 'object'){
-        result[key] = fnDeepClone(obj[key]);//如果对象的属性值为object的时候，递归调用deepClone，即再把某个值对象复制一份到新的对象的对应值中
+        result[key] = fnDeepClone(obj[key]);
       }else{
-        result[key] = obj[key];//如果对象的属性值不为object的时候，直接复制参数对象的每一个键/值到新对象对应的键/值中
+        result[key] = obj[key];
       }
 
     }
